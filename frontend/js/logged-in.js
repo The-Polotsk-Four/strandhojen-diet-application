a92f74530ba4d32a2a3fc51d7f9d3f8b08ec61fd
SofusVingaard <sovi0003@stud.ek.dk>
2025-12-05 10:42:17 +0100

    removed some lines

--- frontend/js/logged-in.js
+++ frontend/js/logged-in.js
@@ -3,7 +3,6 @@ import { getCurrentUser} from "./fetchUser.js";
 window.addEventListener("DOMContentLoaded", initApp);
 
 
-// const API_BASE = 'http://localhost:8080';
 
 async function initApp() {
     const user = await getCurrentUser();
@@ -15,32 +14,6 @@ async function initApp() {
     }
 }
 
-
-
-// export async function getCurrentUser() {
-//     try {
-//         const response = await fetch(`${API_BASE}/api/users/current`, {
-//             method: 'GET',
-//             credentials: 'include',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//
-//         if (response.ok) {
-//             const user = await response.json();
-//             return user;
-//         } else if (response.status === 401 || response.status === 403) {
-//             return null;
-//         } else {
-//             console.error('Failed to get current user:', response.status);
-//             return null;
-//         }
-//     } catch (error) {
-//         console.error('Error getting current user:', error);
-//         return null;
-//     }
-// }
 
 
 
