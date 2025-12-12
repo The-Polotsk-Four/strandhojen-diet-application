42edcd297b08b4f6f3b043d1c17039c532d03a0c
Harning <hejthenerd@gmail.com>
2025-12-12 18:06:35 +0100

    select and get value from allergy list

--- frontend/js/overview.js
+++ frontend/js/overview.js
@@ -1,22 +1,61 @@
 document.addEventListener("DOMContentLoaded", initApp);
 
 const floorSelector = document.querySelector("#floor-selector");
-let residents;
+const allergySelector = document.querySelector("#allergy-selector");
 let activeFloor = -1;
+let allergies
+let residents;
 
 async function initApp() {
     
     floorSelector.addEventListener('input', getFloorValue);
+    allergySelector.addEventListener('input', getAllergyValue);
     
     await fetchResidents();
     console.log(residents);
+    
+    await fetchAllergies();
+    console.log(allergies);
 
     renderResidents();
+    renderAllergies();
     console.log("app initialized");
 }
 
+async function fetchAllergies() {
+    console.log("fetching allergies..");
+    const response = await fetch("http://localhost:8080/api/allergies", {
+        credentials: "include",
+    });
+    if (!response.ok) {
+        console.log("Error: " + response.status);
+        return;
+    }
+    allergies = await response.json()
+}
+
+function renderAllergies() {
+    allergies.forEach(allergy => {
+        renderAllergy(allergy)
+    });
+}
+
+function renderAllergy(allergy) {
+    const option = document.createElement("option");
+    option.value = allergy.id;
+    option.innerHTML = allergy.name;
+    allergySelector.append(option);
+}
+
+function getAllergyValue() {
+    console.log(allergySelector.selectedOptions);
+    for (let option in allergySelector.selectedOptions) { 
+        console.log(allergySelector.selectedOptions[option].value);
+    }
+}
+
 async function fetchResidents() {
-    console.log("fetching..");
+    console.log("fetching residents..");
     const response = await fetch("http://localhost:8080/api/residents", {
         credentials: "include",
     });
@@ -28,8 +67,8 @@ async function fetchResidents() {
 }
 
 function renderResidents() {
-    console.log("residents in resident renderer:");
-    console.log(residents);
+    // console.log("residents in resident renderer:");
+    // console.log(residents);
     residents.forEach(resident => {
         renderResident(resident)
     });
@@ -64,11 +103,12 @@ function renderResidentCell(value) {
 function getFloorValue(event) {
     activeFloor = Number(floorSelector.value);
     console.log(activeFloor);
+    document.querySelector("#floor-selector").selectedIndex = 0;
     document.querySelector("#solid-table").innerHTML = '';
-
     document.querySelector("#soft-table").innerHTML = '';
     document.querySelector("#tube-table").innerHTML = '';
     renderResidents();
+    
 
     console.log(event.target);
 }
