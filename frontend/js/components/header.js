27c1d71b7035f18b62440b3b9251f32aa22a3800
GusViking <guwi0001@stud.ek.dk>
2025-12-17 10:46:55 +0100

    I made it so now you can go between each side in the header.js

--- frontend/js/components/header.js
+++ frontend/js/components/header.js
@@ -4,14 +4,14 @@ class HeaderComponent extends HTMLElement {
      <header className="header">
     <nav>
         <ul>
-            <li><a href="/html/index.html">Hjem</a></li>
-            <li><a href="/html/resident.create.html">Opret beboer</a></li>
-            <li><a href="/html/resident-update.html">Opdater beboer </a></li>
-            <li><a href="/html/resident-find.html">Søg efter beboer</a></li>
-            <li><a href="/html/resident-delete.html">Slet beboer</a></li>
-            <li><a href="/html/create-user.html">Opret bruger</a></li>
-            <li><a href="/html/delete-user-.html">Slet bruger</a></li>
-            <li><a href="/html/resident-add-allergies.html">Rediger allergier</a></li>
+            <li><a href="../html/index.html">Hjem</a></li>
+            <li><a href="../html/resident.create.html">Opret beboer</a></li>
+            <li><a href="../html/resident-update.html">Opdater beboer </a></li>
+            <li><a href="../html/resident-find.html">Søg efter beboer</a></li>
+            <li><a href="../html/resident-delete.html">Slet beboer</a></li>
+            <li><a href="../html/create-user.html">Opret bruger</a></li>
+            <li><a href="../html/delete-user-.html">Slet bruger</a></li>
+            <li><a href="../html/resident-add-allergies.html">Rediger allergier</a></li>
 
         </ul>
     </nav>
