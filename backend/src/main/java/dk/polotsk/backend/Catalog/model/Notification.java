59c9bbceb4b7f4858cc7a8072b48d6bc0f4dad90
SofusVingaard <Sovi0003@stud.ek.dk>
2025-12-19 12:16:36 +0100

    Reorganize imports in Notification.java

--- backend/src/main/java/dk/polotsk/backend/Catalog/model/Notification.java
+++ backend/src/main/java/dk/polotsk/backend/Catalog/model/Notification.java
@@ -1,9 +1,12 @@
 package dk.polotsk.backend.Catalog.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+
 
 import java.time.LocalDateTime;
 
