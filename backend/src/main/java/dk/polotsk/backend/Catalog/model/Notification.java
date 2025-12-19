309e623a9c8bc48a903b2bfd4265b0f14ec92f72
SofusVingaard <Sovi0003@stud.ek.dk>
2025-12-19 10:54:41 +0100

    Change ID generation strategy to IDENTITY

--- backend/src/main/java/dk/polotsk/backend/Catalog/model/Notification.java
+++ backend/src/main/java/dk/polotsk/backend/Catalog/model/Notification.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class Notification {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String message;
